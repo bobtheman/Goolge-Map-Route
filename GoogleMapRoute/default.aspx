7ec32f725303e3657f31257d94a0117b844c531c
bobtheman <rob_owen@outlook.com>
2017-08-25 15:07:35 +0100

    Add files via upload

---
+++ GoogleMapRoute/default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="GoogleMapRoute._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			<table>
+				<tr>
+					<td>From</td>
+					<td><asp:TextBox runat="server" ID="txtfrom" CssClass="form-control" ValidationGroup="googledirections"></asp:TextBox></td>
+					<td></td>
+					<td>To</td>
+					<td><asp:TextBox runat="server" ID="txtto" CssClass="form-control" ValidationGroup="googledirections"></asp:TextBox></td>
+					<td></td>
+					<td><asp:Button runat="server" ID="btnGetDirections" OnClick="btnGetDirections_Click" CausesValidation="true" Text="Get Directions" ValidationGroup="googledirections" /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><asp:RequiredFieldValidator runat="server" ID="reqtxtfrom" ControlToValidate="txtfrom" Display="Dynamic" SetFocusOnError="true" ErrorMessage="*" ForeColor="Red"></asp:RequiredFieldValidator></td>
+					<td></td>
+					<td><asp:RequiredFieldValidator runat="server" ID="reqtxtto" ControlToValidate="txtfrom" Display="Dynamic" SetFocusOnError="true" ErrorMessage="*" ForeColor="Red"></asp:RequiredFieldValidator></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Duration:</td>
+					<td><asp:Label runat="server" ID="lblDuration"></asp:Label></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Distance:</td>
+					<td><asp:Label runat="server" ID="lblDistance"></asp:Label></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GoogleMapRoute/default.aspx.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GoogleMapRoute
+{
+	public partial class _default : System.Web.UI.Page
+	{
+		string GoogleKey = "AIzaSyDN4JQdz3RONNI-LiFDp0iIUivZf3EnUPA";
+		//string GoogleKey = "-- Your Key Here --";
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnGetDirections_Click(object sender, EventArgs e)
+		{
+			GetGoogleDirections(txtfrom.Text.Trim(), txtto.Text.Trim());
+		}
+
+		protected void GetGoogleDirections(string From, string To)
+		{
+			try
+			{
+				string GoogleUrl = ("https://maps.googleapis.com/maps/api/directions/json?units=imperial&origin=" + (From.Trim()) + ("&destination=") + (To.Trim()) + (",&key=") + (GoogleKey));
+				var result = new System.Net.WebClient().DownloadString(GoogleUrl);
+				var root = JsonConvert.DeserializeObject<MapDirections>(result);
+				lblDistance.Text = root.routes[0].legs[0].distance.text;
+				lblDuration.Text = root.routes[0].legs[0].duration.text;
+			}
+			catch (Exception ex)
+			{
+				string ErrorMessage = ex.ToString();
+			}
+
+		}
+		public class MapDirections
+		{
+			public IList<GeocodedWaypoint> geocoded_waypoints { get; set; }
+			public IList<Route> routes { get; set; }
+			public string status { get; set; }
+		}
+
+		public class GeocodedWaypoint
+		{
+			public string geocoder_status { get; set; }
+			public string place_id { get; set; }
+			public IList<string> types { get; set; }
+		}
+
+		public class Route
+		{
+			public string copyrights { get; set; }
+			public IList<Leg> legs { get; set; }
+			public string summary { get; set; }
+			public IList<object> warnings { get; set; }
+			public IList<object> waypoint_order { get; set; }
+		}
+
+		public class Leg
+		{
+			public Distance distance { get; set; }
+			public Duration duration { get; set; }
+			public string end_address { get; set; }
+			public EndLocation end_location { get; set; }
+			public string start_address { get; set; }
+			public StartLocation start_location { get; set; }
+			public IList<object> traffic_speed_entry { get; set; }
+			public IList<object> via_waypoint { get; set; }
+		}
+
+		public class Distance
+		{
+			public string text { get; set; }
+			public int value { get; set; }
+		}
+
+		public class Duration
+		{
+			public string text { get; set; }
+			public int value { get; set; }
+		}
+
+		public class EndLocation
+		{
+			public double lat { get; set; }
+			public double lng { get; set; }
+		}
+
+		public class StartLocation
+		{
+			public double lat { get; set; }
+			public double lng { get; set; }
+		}
+
+	}
+}
